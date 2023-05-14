d4a885c8994eaf8e7ff035266d6c7a1c1215be74
pettijohn <632844+pettijohn@users.noreply.github.com>
2023-05-13 17:05:27 -0700

    Publish via dotnet public rather than Dockerfile

--- publish.sh
+++ publish.sh
@@ -7,3 +7,8 @@ docker image push ghcr.io/pettijohn/asus-driver-feed:latest-arm64
 dotnet publish --os linux --arch x64 /t:PublishContainer -c Release
 docker image tag asus-driver-feed:1.0.0 ghcr.io/pettijohn/asus-driver-feed:latest-amd64
 docker image push ghcr.io/pettijohn/asus-driver-feed:latest-amd64
+
+# Merge both into a multi-platform image
+docker buildx imagetools create --tag ghcr.io/pettijohn/asus-driver-feed:latest \
+    ghcr.io/pettijohn/asus-driver-feed:latest-arm64 \
+    ghcr.io/pettijohn/asus-driver-feed:latest-amd64

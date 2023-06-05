47a506897d05babd130c5b62995c3f70ec00e85b
pettijohn <632844+pettijohn@users.noreply.github.com>
2023-06-05 11:37:18 -0700

    AMD Chipset/iGPU drivers (draft)

--- src/Program.cs
+++ src/Program.cs
@@ -2,6 +2,7 @@ using System.Net;
 using System.ServiceModel.Syndication;
 using System.Text;
 using System.Text.Json;
+using System.Text.RegularExpressions;
 using System.Xml;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -33,8 +34,15 @@ app.Run();
 
 async Task<IResult> GenerateFeed()
 {
-    var http = new HttpClient();
-    
+    HttpClientHandler handler = new HttpClientHandler()
+    {
+        AutomaticDecompression = DecompressionMethods.GZip | DecompressionMethods.Deflate | DecompressionMethods.Brotli
+    };
+    var http = new HttpClient(handler);
+    http.DefaultRequestHeaders.Add("Accept", "*/*");
+    http.DefaultRequestHeaders.Add("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36 Edg/114.0.1823.37");
+    http.DefaultRequestHeaders.Add("Connection", "keep-alive");
+
     var feed = new SyndicationFeed("ASUS Driver Feed", "Live updates of ASUS drivers", new Uri("https://rog.asus.com/support"));
     feed.TimeToLive = TimeSpan.FromHours(12);
     feed.LastUpdatedTime = DateTimeOffset.UtcNow;
@@ -77,6 +85,37 @@ async Task<IResult> GenerateFeed()
         }
     }
 
+    // AMD
+    // https://www.amd.com/en/support
+    // <a href="https://drivers.amd.com/drivers/installer/22.40/whql/amd-software-adrenalin-edition-23.5.2-minimalsetup-230531_web.exe">DOWNLOAD WINDOWS DRIVERS </a></div>
+    if(true)
+    {
+        var amdUrl = "https://www.amd.com/en/support";
+        
+        // Very picky server - will just hang without headers being just so.
+        // Need a timeout handler that also emits a SyndicationItem
+        var body = await http.GetStringAsync(amdUrl);
+
+        
+        var match = Regex.Match(body, @"href=""([^""]+)"">DOWNLOAD WINDOWS DRIVERS", RegexOptions.IgnoreCase | RegexOptions.Multiline);
+        var downloadUrl = match.Groups[1].Value;
+        var version = Regex.Match(downloadUrl, @"(\d+\.\d+\.\d+)").Groups[1].Value;
+
+        
+        var headReq = new HttpRequestMessage(HttpMethod.Head, downloadUrl);
+        headReq.Headers.Add("Accept", http.DefaultRequestHeaders.Accept.ToString());
+        headReq.Headers.Add("User-Agent", "PostmanRuntime/7.32.2");
+        headReq.Headers.Add("Connection", http.DefaultRequestHeaders.Connection.ToString());
+        var headResp = await http.SendAsync(headReq);
+        var lastModified = DateTimeOffset.FromUnixTimeSeconds(Int64.Parse(headResp.Headers.ETag!.Tag.Trim('"')));
+
+        var item = new SyndicationItem(System.Net.WebUtility.UrlDecode("AMD Windows Drivers") + " - " + version, 
+                $"Version {version}", new Uri(downloadUrl), downloadUrl, 
+                lastModified);
+            
+
+        items.Add(item);
+    }
 
     feed.Items = items;
 
